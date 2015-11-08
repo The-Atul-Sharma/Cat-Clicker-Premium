920523c3d44440292115d47f90b41345887b3831
Atul Sharma <theatsharma@gmail.com>
2015-11-08 22:01:32 +0530

    Proect Cat Clicker Premium

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$('#li1').click(function(){
+	$('.container').empty();
+	$('.container').append('<div id="1"><h1>KITTY</h1><img src="img/Cat1.jpg"><h2 id="a"></h2></div>');
+	var count = 0;
+	$('#1').click(function(){
+		count++;
+		$('#a').text(count);
+	});
+});
+$('#li2').click(function(){
+	$('.container').empty();
+	$('.container').append('<div id="1"><h1>LUCY</h1><img src="img/Cat2.jpg"><h2 id="a"></h2></div>');
+	var count = 0;
+	$('#1').click(function(){
+		count++;
+		$('#a').text(count);
+	});
+});
+$('#li3').click(function(){
+	$('.container').empty();
+	$('.container').append('<div id="1"><h1>SASA</h1><img src="img/Cat3.jpg"><h2 id="a"></h2></div>');
+	var count = 0;
+	$('#1').click(function(){
+		count++;
+		$('#a').text(count);
+	});
+});
+$('#li4').click(function(){
+	$('.container').empty();
+	$('.container').append('<div id="1"><h1>BELLA</h1><img src="img/Cat4.jpg"><h2 id="a"></h2></div>');
+	var count = 0;
+	$('#1').click(function(){
+		count++;
+		$('#a').text(count);
+	});
+});
+$('#li5').click(function(){
+	$('.container').empty();
+	$('.container').append('<div id="1"><h1>MISSY</h1><img src="img/Cat5.jpg"><h2 id="a"></h2></div>');
+	var count = 0;
+	$('#1').click(function(){
+		count++;
+		$('#a').text(count);
+	});
+});
