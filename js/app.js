f93631f1b3ef9fbd1a6b5faf7d0e933bb216d7a2
Atul Sharma <theatsharma@gmail.com>
2015-11-10 00:42:49 +0530

    Use for loop

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
-$('#li1').click(function(){
+var count = [0,0,0,0,0,0];
+for(var i = 1;i <= 5;i++){
+	$('#li'+i).click(function(i){
+		return function(){
+			$('.container').empty();
+			$('.container').append('<div id="1"><h1>KITTY</h1><img src="img/Cat'+i+'.jpg"><h2 id="a"></h2></div>');
+			$('#a').text(count[i]);
+			$('#1').click(function(){
+			count[i]++;
+			$('#a').text(count[i]);
+			});
+		}
+	}(i));
+}
+/*$('#li1').click(function(){
 	$('.container').empty();
 	$('.container').append('<div id="1"><h1>KITTY</h1><img src="img/Cat1.jpg"><h2 id="a"></h2></div>');
 	var count = 0;
@@ -42,4 +56,4 @@ $('#li5').click(function(){
 		count++;
 		$('#a').text(count);
 	});
-});
+});*/
